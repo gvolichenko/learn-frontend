2356db79405f230eb4ff7a2e7dddda9c29f4ca76
George Volichenko <georgevolichenko@Georges-MacBook-Pro.local>
2020-08-14 20:08:24 -0700

    old weather app functionality working with webpack

---
+++ capstone-travel-app/src/client/index.js
@@ -0,0 +1,7 @@
+import './styles/style.scss'
+
+import { travelApp  } from "./js/app.js";
+
+travelApp();
+
+export{travelApp};
---
+++ capstone-travel-app/src/client/js/app.js
@@ -0,0 +1,94 @@
+
+const travelApp = () => {
+    /* Global Variables */
+    const baseUrl='http://api.openweathermap.org/data/2.5/weather?zip=';
+    const apiKey='76a469593954e6e2dc828c770a16ff2f';
+    // Create a new date instance dynamically with JS
+    let d = new Date();
+    // need to add 1 to month to make it 1-12 instead of 0-11
+    let newDate = (d.getMonth()+1) + '.'+ d.getDate()+'.'+ d.getFullYear();
+
+    document.getElementById('generate').addEventListener('click', performAction);
+
+
+    // Async GET from OPW API
+    const pullWeather = async (baseurl,zip,apikey) => {
+        const weather = await fetch(baseurl+zip+'&appid='+apikey);
+        try{
+            const weatherJSON = await weather.json();
+            return weatherJSON;
+        }
+        catch(error){console.log('ERROR!',error)}
+    };
+
+    // Async POST to my server
+    const postWeather = async (url = '', dataObj)=>{
+        // API returns temp in Kelvin for some reason ¯\_(ツ)_/¯
+        const userFeeling =  document.getElementById('feelings').value;
+        const toPost = {temperature:(Math.round(dataObj.main.temp - 273.15)) , 
+                date:newDate, 
+                city: dataObj.name,
+                userResponse:userFeeling};
+            console.log(url);
+        const response = await fetch(url, {
+            method: 'POST', 
+            credentials: 'same-origin', 
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(toPost), // body data type must match "Content-Type" header   
+        });
+        try {
+            const newDataObj = await response.json();
+            return newDataObj;
+        }catch(error) {
+        console.log("error", error);
+        }
+
+    };
+
+    const retrieveAndUpdate = async () => {
+        const request = await fetch('http://localhost:3000/retrieve');
+            try{
+                const recentData = await request.json()
+                document.getElementById('date').innerHTML = 
+                `DATE: ${recentData.date}`;
+                document.getElementById('city').innerHTML = 
+                `CITY: ${recentData.city}`;
+                document.getElementById('temp').innerHTML = 
+                `TEMP (C): ${recentData.temperature}`;
+                document.getElementById('content').innerHTML = 
+                `USER INPUT: ${recentData.userResponse}`;
+            }
+            catch(error) {
+                console.log("error",error);
+            }
+    };
+
+    function performAction(e){
+        const newZip =  document.getElementById('zip').value;
+        // Needs a vaid zip code for the API call.
+        if(!isUsZip(newZip)){
+            alert('Please enter a valid US zipcode.');
+        }
+        // chaining promises below
+        pullWeather(baseUrl,newZip,apiKey)
+        .then(function(data){
+            postWeather('http://localhost:3000/add',data);
+        })
+        .then(function(){
+            retrieveAndUpdate();
+        }
+        )
+    };
+
+    function isUsZip(str)
+    {
+    const regexp = /^[0-9]{5}(?:-[0-9]{4})?$/;
+    
+            if (regexp.test(str)){return true;}
+            else{return false;}
+    };
+};
+
+export {travelApp};
